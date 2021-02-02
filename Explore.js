65805eae62796221fcde380d5bf17e3421bb768c
Nicholas T. Smith <nicholastoddsmith@gmail.com>
2021-02-01 20:01:41 -0800

    Initial Commit

---
+++ Explore.js
@@ -0,0 +1,176 @@
+var layOut = {"font": {"family": "Times New Roman"}, "paper_bgcolor": "rgba(0,0,0,0)", "plot_bgcolor": "rgba(0,0,0,0)", "template": {"data": {"bar": [{"error_x": {"color": "#f2f5fa"}, "error_y": {"color": "#f2f5fa"}, "marker": {"line": {"color": "rgb(17,17,17)", "width": 0.5}}, "type": "bar"}], "barpolar": [{"marker": {"line": {"color": "rgb(17,17,17)", "width": 0.5}}, "type": "barpolar"}], "carpet": [{"aaxis": {"endlinecolor": "#A2B1C6", "gridcolor": "#506784", "linecolor": "#506784", "minorgridcolor": "#506784", "startlinecolor": "#A2B1C6"}, "baxis": {"endlinecolor": "#A2B1C6", "gridcolor": "#506784", "linecolor": "#506784", "minorgridcolor": "#506784", "startlinecolor": "#A2B1C6"}, "type": "carpet"}], "choropleth": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "type": "choropleth"}], "contour": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "colorscale": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]], "type": "contour"}], "contourcarpet": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "type": "contourcarpet"}], "heatmap": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "colorscale": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]], "type": "heatmap"}], "heatmapgl": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "colorscale": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]], "type": "heatmapgl"}], "histogram": [{"marker": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "histogram"}], "histogram2d": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "colorscale": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]], "type": "histogram2d"}], "histogram2dcontour": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "colorscale": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]], "type": "histogram2dcontour"}], "mesh3d": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "type": "mesh3d"}], "parcoords": [{"line": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "parcoords"}], "pie": [{"automargin": true, "type": "pie"}], "scatter": [{"marker": {"line": {"color": "#283442"}}, "type": "scatter"}], "scatter3d": [{"line": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "marker": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "scatter3d"}], "scattercarpet": [{"marker": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "scattercarpet"}], "scattergeo": [{"marker": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "scattergeo"}], "scattergl": [{"marker": {"line": {"color": "#283442"}}, "type": "scattergl"}], "scattermapbox": [{"marker": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "scattermapbox"}], "scatterpolar": [{"marker": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "scatterpolar"}], "scatterpolargl": [{"marker": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "scatterpolargl"}], "scatterternary": [{"marker": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "type": "scatterternary"}], "surface": [{"colorbar": {"outlinewidth": 0, "ticks": ""}, "colorscale": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]], "type": "surface"}], "table": [{"cells": {"fill": {"color": "#506784"}, "line": {"color": "rgb(17,17,17)"}}, "header": {"fill": {"color": "#2a3f5f"}, "line": {"color": "rgb(17,17,17)"}}, "type": "table"}]}, "layout": {"annotationdefaults": {"arrowcolor": "#f2f5fa", "arrowhead": 0, "arrowwidth": 1}, "autotypenumbers": "strict", "coloraxis": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "colorscale": {"diverging": [[0, "#8e0152"], [0.1, "#c51b7d"], [0.2, "#de77ae"], [0.3, "#f1b6da"], [0.4, "#fde0ef"], [0.5, "#f7f7f7"], [0.6, "#e6f5d0"], [0.7, "#b8e186"], [0.8, "#7fbc41"], [0.9, "#4d9221"], [1, "#276419"]], "sequential": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]], "sequentialminus": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]]}, "colorway": ["#636efa", "#EF553B", "#00cc96", "#ab63fa", "#FFA15A", "#19d3f3", "#FF6692", "#B6E880", "#FF97FF", "#FECB52"], "font": {"color": "#f2f5fa"}, "geo": {"bgcolor": "rgb(17,17,17)", "lakecolor": "rgb(17,17,17)", "landcolor": "rgb(17,17,17)", "showlakes": true, "showland": true, "subunitcolor": "#506784"}, "hoverlabel": {"align": "left"}, "hovermode": "closest", "mapbox": {"style": "dark"}, "paper_bgcolor": "rgb(17,17,17)", "plot_bgcolor": "rgb(17,17,17)", "polar": {"angularaxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}, "bgcolor": "rgb(17,17,17)", "radialaxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}}, "scene": {"xaxis": {"backgroundcolor": "rgb(17,17,17)", "gridcolor": "#506784", "gridwidth": 2, "linecolor": "#506784", "showbackground": true, "ticks": "", "zerolinecolor": "#C8D4E3"}, "yaxis": {"backgroundcolor": "rgb(17,17,17)", "gridcolor": "#506784", "gridwidth": 2, "linecolor": "#506784", "showbackground": true, "ticks": "", "zerolinecolor": "#C8D4E3"}, "zaxis": {"backgroundcolor": "rgb(17,17,17)", "gridcolor": "#506784", "gridwidth": 2, "linecolor": "#506784", "showbackground": true, "ticks": "", "zerolinecolor": "#C8D4E3"}}, "shapedefaults": {"line": {"color": "#f2f5fa"}}, "sliderdefaults": {"bgcolor": "#C8D4E3", "bordercolor": "rgb(17,17,17)", "borderwidth": 1, "tickwidth": 0}, "ternary": {"aaxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}, "baxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}, "bgcolor": "rgb(17,17,17)", "caxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}}, "title": {"x": 0.05}, "updatemenudefaults": {"bgcolor": "#506784", "borderwidth": 0}, "xaxis": {"automargin": true, "gridcolor": "#283442", "linecolor": "#506784", "ticks": "", "title": {"standoff": 15}, "zerolinecolor": "#283442", "zerolinewidth": 2}, "yaxis": {"automargin": true, "gridcolor": "#283442", "linecolor": "#506784", "ticks": "", "title": {"standoff": 15}, "zerolinecolor": "#283442", "zerolinewidth": 2}}}, "title": {"font": {"size": 30}, "text": "SARS-CoV-2 Genome Explorer"}, "xaxis": {"gridcolor": "#909090", "title": {"font": {"size": 20}, "text": "Principal Axis #1"}, "zerolinecolor": "#909090"}, "yaxis": {"gridcolor": "#909090", "title": {"font": {"size": 20}, "text": "Principal Axis #2"}, "zerolinecolor": "#909090"},
+  margin: {r: 20, b: 8, pad: 4}};
+
+var barLayout = {"font": {"family": "Times New Roman"}, "paper_bgcolor": "rgba(0,0,0,0)", "plot_bgcolor": "rgba(0,0,0,0)",
+
+"template": {"data": {"bar": [{"error_x": {"color": "#f2f5fa"}, "error_y": {"color": "#f2f5fa"}, "marker": {"line": {"color": "rgb(220, 220, 250)", "width": 0.5}}, "type": "bar"}]},
+  "layout": {"annotationdefaults": {"arrowcolor": "#f2f5fa", "arrowhead": 0, "arrowwidth": 1}, "autotypenumbers": "strict", "coloraxis": {"colorbar": {"outlinewidth": 0, "ticks": ""}}, "colorscale": {"diverging": [[0, "#8e0152"], [0.1, "#c51b7d"], [0.2, "#de77ae"], [0.3, "#f1b6da"], [0.4, "#fde0ef"], [0.5, "#f7f7f7"], [0.6, "#e6f5d0"], [0.7, "#b8e186"], [0.8, "#7fbc41"], [0.9, "#4d9221"], [1, "#276419"]], "sequential": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]], "sequentialminus": [[0.0, "#0d0887"], [0.1111111111111111, "#46039f"], [0.2222222222222222, "#7201a8"], [0.3333333333333333, "#9c179e"], [0.4444444444444444, "#bd3786"], [0.5555555555555556, "#d8576b"], [0.6666666666666666, "#ed7953"], [0.7777777777777778, "#fb9f3a"], [0.8888888888888888, "#fdca26"], [1.0, "#f0f921"]]}, "colorway": ["#636efa", "#EF553B", "#00cc96", "#ab63fa", "#FFA15A", "#19d3f3", "#FF6692", "#B6E880", "#FF97FF", "#FECB52"], "font": {"color": "#f2f5fa"}, "geo": {"bgcolor": "rgb(17,17,17)", "lakecolor": "rgb(17,17,17)", "landcolor": "rgb(17,17,17)", "showlakes": true, "showland": true, "subunitcolor": "#506784"}, "hoverlabel": {"align": "left"}, "hovermode": "closest", "mapbox": {"style": "dark"}, "paper_bgcolor": "rgb(17,17,17)", "plot_bgcolor": "rgb(17,17,17)", "polar": {"angularaxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}, "bgcolor": "rgb(17,17,17)", "radialaxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}}, "scene": {"xaxis": {"backgroundcolor": "rgb(17,17,17)", "gridcolor": "#506784", "gridwidth": 2, "linecolor": "#506784", "showbackground": true, "ticks": "", "zerolinecolor": "#C8D4E3"}, "yaxis": {"backgroundcolor": "rgb(17,17,17)", "gridcolor": "#506784", "gridwidth": 2, "linecolor": "#506784", "showbackground": true, "ticks": "", "zerolinecolor": "#C8D4E3"}, "zaxis": {"backgroundcolor": "rgb(17,17,17)", "gridcolor": "#506784", "gridwidth": 2, "linecolor": "#506784", "showbackground": true, "ticks": "", "zerolinecolor": "#C8D4E3"}}, "shapedefaults": {"line": {"color": "#f2f5fa"}}, "sliderdefaults": {"bgcolor": "#C8D4E3", "bordercolor": "rgb(17,17,17)", "borderwidth": 1, "tickwidth": 0}, "ternary": {"aaxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}, "baxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}, "bgcolor": "rgb(17,17,17)", "caxis": {"gridcolor": "#506784", "linecolor": "#506784", "ticks": ""}}, "title": {"x": 0.05}, "updatemenudefaults": {"bgcolor": "#506784", "borderwidth": 0}, "xaxis": {"automargin": true, "gridcolor": "#283442", "linecolor": "#506784", "ticks": "", "title": {"standoff": 15}, "zerolinecolor": "#283442", "zerolinewidth": 2}}}, "title": {'visible': false}, "xaxis": {'ticksuffix': 'b'}, margin: {l: 60, r: 10, b: 0, t: 10, pad: 4}, 
+"yaxis": {'showgrid': false, 'zeroline': false, "title": {"font": {"size": 16}, "text": "Variant Count"}}};
+
+function DateToString(d) {
+    return NPad(d.getMonth() + 1) + "/" + NPad(d.getDate()) + "/" + NPad(d.getFullYear());
+}
+
+function GetData(s, e) {
+  var i1 = DAT['DI'][s];
+  var i2 = DAT['DI'][e];
+  return {'X': DAT['X'].slice(i1, i2),
+          'Y': DAT['Y'].slice(i1, i2),
+          'S': DAT['S'].slice(i1, i2),
+          'C': DAT['C'].slice(i1, i2),
+          'O': DAT['O'].slice(i1, i2),
+          'L': DAT['L'].slice(i1, i2),
+          'D': DAT['D'].slice(i1, i2),
+         };
+}
+
+function LoadJSON() {   
+
+  var xObj = new XMLHttpRequest();
+  xObj.overrideMimeType("application/json");
+  xObj.open('GET', 'DataDump.json', true);
+  xObj.onreadystatechange = function () {
+        if (xObj.readyState == 4 && xObj.status == "200") {
+          DAT = JSON.parse(xObj.responseText);
+          X = DAT['MP'];
+          Y = Array(X.length);
+          OnLoad();
+        }
+  };
+  xObj.send(null);  
+}
+
+function NPad(n) {
+    return ("00" + String(n)).slice(-2)
+}
+
+function OnLoad() {
+
+  var off = UpdateRangeBar();
+  if (off === null)
+    return;
+
+  var ld = GetData(off[0], off[1]);
+
+  var xd = (DAT['x1'] - DAT['x0']) * 0.05;
+  var yd = (DAT['y1'] - DAT['y0']) * 0.05;
+  layOut['xaxis']['range'] = [DAT['x0'] - xd, DAT['x1'] + xd];
+  layOut['yaxis']['range'] = [DAT['y0'] - yd, DAT['y1'] + yd];
+
+  Plotly.newPlot('graph', [{
+    x: ld['X'], y: ld['Y'], marker: {size: ld['S'], color: ld['C']},
+    mode: 'markers',
+    type: 'scatter',
+    text: ld['L']
+  }], layOut, {responsive: true});
+
+  UpdateBar(off[0], off[1]);
+
+  Plotly.newPlot('mutBar', [{x: X, y: Y, type: 'bar'}], barLayout, {displayModeBar: false, responsive: true});
+}
+
+function Update() {
+
+  var off = UpdateRangeBar();
+  if (off === null)
+    return;
+
+  var ld = GetData(off[0], off[1]);
+
+  Plotly.animate('graph', {
+    data: [{x: ld['X'],
+            y: ld['Y'],
+            marker: {size: ld['S'], color: ld['C']},
+            text: ld['L'],
+            layout: layOut}],
+    traces: [0]
+  }, {
+    transition: {
+      duration: 1234
+    },
+    frame: {
+      duration: 1234
+    }
+  });
+
+  UpdateBar(off[0], off[1]);
+
+  var xCnt = Math.max(...Y);
+  var cCnt = (barLayout['yaxis']['range'] || [0, 0])[1];
+
+  if ((xCnt > cCnt) || (((xCnt - cCnt) / cCnt) < -0.33)) {
+    barLayout['yaxis']['range'] = [0, xCnt];
+  }
+
+  Plotly.animate('mutBar', {
+    data: [{x: X, y: Y}],
+    traces: [0],
+    layout: barLayout
+  }, {
+    transition: {
+      duration: 1234,
+      easing: 'linear'
+    },
+    frame: {
+      duration: 1234
+    }
+  });
+}
+
+function UpdateRangeBar() {
+  var tl1 = document.getElementById('rangeTitle1');
+  var tl2 = document.getElementById('rangeTitle2');
+  var rb1 = document.getElementById('dateRange1');
+  var rb2 = document.getElementById('dateRange2');
+
+  if ((tl1 === null) || (tl2 === null) || (rb1 === null) || (rb2 === null) || (DAT === null))
+    return null;
+
+  // Last element is sentinel element
+  var maxVal = DAT['DI'].length - 2;
+  var maxRow = DAT['D'].length - 1;
+
+  rb2.max = rb1.max = maxVal;
+  var v1 = Math.min(rb1.value, maxVal);
+  var v2 = Math.min(rb2.value, maxVal);
+  rb1.value = v1 = Math.min(v1, v2);
+  rb2.value = v2 = Math.max(v1, v2) + 1;
+
+  var s = DAT['DI'][v1];
+  var e = Math.min(DAT['DI'][v2 - 1], maxRow);  // Display date as inclusive
+
+  tl1.innerHTML = DAT['D'][s];
+  tl2.innerHTML = DAT['D'][e];
+
+  ele = document.getElementById("numDaysText");
+  if (ele !== null) {
+    ele.innerHTML = Math.round((Date.parse(DAT['D'][e]) - Date.parse(DAT['D'][s])) / (1000 * 60 * 60 * 24) + 1) + " Days";
+  }
+
+  // Now use exclusive ending logic
+  e = Math.min(DAT['DI'][v2], maxRow);
+  var ele = document.getElementById("numSampText");
+  if (ele !== null) {
+    ele.innerHTML = (e - s) + " Samples";
+  }
+
+  ele = document.getElementById("pctSampText");
+  if (ele !== null) {
+    ele.innerHTML = Math.round(((e - s) / maxRow) * 100) + "%";
+  }
+
+  return [v1, v2];
+}
+
+function UpdateBar(s, e) {
+  var v1 = DAT['MF'][s];
+  var v2 = DAT['MF'][e];
+  for(var i = 0; i < X.length; i++) {
+    Y[i] = v2[i] - v1[i];
+  }
+}
+
+var DAT = null;
+var Y = null;
+var X = null;
+LoadJSON();
